748e347484373f0afde0270a537111be2e28aad4
wcheung8 <we_cheung@yahoo.com>
2016-11-23 15:03:08 -0500

    added sql to login and register

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -1,16 +1,68 @@
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.sql.*;
+
+import fxapp.Main;
 
 public class LoginController extends Controller {
+
+	//driver details
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	static final String DB_URL = "jdbc:mysql://academic-mysql.cc.gatech.edu/cs4400_Team_1";
 	
 	@FXML
-	public void handleLoginPressed() {
-		showScreen("../view/AdminMainScreen.fxml", "Main Screen");
-	    
-	    
-	}
+	TextField userField;
 	
+	@FXML
+	PasswordField passwordField;
+
+	@FXML
+	public void handleLoginPressed() {
+		Connection conn = null;
+	    Statement stmt = null;
+	    
+	    String username = userField.getText();
+	    String password = passwordField.getText();
+	    
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+
+			conn = DriverManager.getConnection(DB_URL, "cs4400_Team_1", "MONLSe9e");
+			stmt = conn.createStatement();
+			String sql;
+			
+			sql = "SELECT password FROM USER where username = '" + username+ "';";
+			ResultSet rs = stmt.executeQuery(sql);
+
+			rs.next();
+			
+			if(password.equals(rs.getString("password"))){
+				showScreen("../view/MainScreen.fxml", "Main Screen");
+			} else {
+	            Alert alert = new Alert(Alert.AlertType.ERROR);
+	            alert.initOwner(Main.stage());
+	            alert.setTitle("User Not Found");
+	            alert.setHeaderText("User Not Found");
+	            alert.setContentText("Username or password incorrect");
+	            alert.show();
+	        }
+			
+			rs.close();
+			stmt.close();
+			conn.close();
+			
+		} catch (SQLException se) {
+			se.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 	@FXML
 	public void handleRegisterPressed() {
 	    showScreen("../view/RegisterScreen.fxml", "Register");
--- src/controller/RegisterController.java
+++ src/controller/RegisterController.java
@@ -1,16 +1,88 @@
 package controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import fxapp.Main;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class RegisterController extends Controller {
+	// driver details
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	static final String DB_URL = "jdbc:mysql://academic-mysql.cc.gatech.edu/cs4400_Team_1";
 
-    @FXML
-    public void handleCreatePressed() {
+	@FXML
+	TextField userField;
 
-    }
+	@FXML
+	TextField emailField;
 
-    @FXML
-    public void handleCancelPressed() {
-        showScreen("../view/LoginScreen.fxml", "Login");
-    }
+	@FXML
+	PasswordField passwordField;
+
+	@FXML
+	PasswordField checkPasswordField;
+
+	@FXML
+	public void handleCreatePressed() {
+
+		Connection conn = null;
+		Statement stmt = null;
+
+		String username = userField.getText();
+		String email = emailField.getText();
+		String password = passwordField.getText();
+		String checkPassword = checkPasswordField.getText();
+		
+		if (!checkPassword.equals(password) ) {
+			Alert alert = new Alert(Alert.AlertType.ERROR);
+			alert.initOwner(Main.stage());
+			alert.setTitle("Passwords must match!");
+			alert.setHeaderText("Passwords must match!");
+			alert.setContentText("Password check did not match.");
+			alert.show();
+		} else {
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+
+				conn = DriverManager.getConnection(DB_URL, "cs4400_Team_1", "MONLSe9e");
+				stmt = conn.createStatement();
+				String sql;
+
+				sql = "INSERT INTO USER (username, password, isAdmin, gtemail) VALUES ('" + username + "','" + password + "', 0,'" + email + "');";
+				int rs = stmt.executeUpdate(sql);
+
+				System.out.println(rs);
+				
+				stmt.close();
+				conn.close();
+				
+				showScreen("../view/LoginScreen.fxml", "Login");
+
+			} catch (SQLException se) {
+				if(se.getMessage().contains("Duplicate")) {
+					Alert alert = new Alert(Alert.AlertType.ERROR);
+					alert.initOwner(Main.stage());
+					alert.setTitle("User already exists!");
+					alert.setHeaderText("User already exists!");
+					alert.setContentText("Cannot insert duplicate user.");
+					alert.show();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	@FXML
+	public void handleCancelPressed() {
+		showScreen("../view/LoginScreen.fxml", "Login");
+	}
 }
