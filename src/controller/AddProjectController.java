a2d624b3488c7c045baa10a6a935ba36c8fb84ed
wcheung8 <we_cheung@yahoo.com>
2016-12-02 16:30:18 -0500

    cleaned up and fixed add course/project errors

--- src/controller/AddProjectController.java
+++ src/controller/AddProjectController.java
@@ -16,223 +16,181 @@ import java.util.ArrayList;
 
 public class AddProjectController extends Controller {
 
-	// driver details
-	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-	static final String DB_URL = "jdbc:mysql://academic-mysql.cc.gatech.edu/cs4400_Team_1";
+    // driver details
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://academic-mysql.cc.gatech.edu/cs4400_Team_1";
 
-	@FXML
-	private TextField projectNameField;
-	@FXML
-	private TextField advisorField;
-	@FXML
-	private TextField advisorEmailField;
-	@FXML
-	private TextArea descriptionField;
-	@FXML
-	private TextField estNumField;
-	@FXML
-	private ComboBox<String> designationField;
-	@FXML
-	private ComboBox<String> majorField;
-	@FXML
-	private ComboBox<String> yearField;
-	@FXML
-	private ComboBox<String> departmentField;
-	@FXML
-	private ListView<String> categoryField;
+    @FXML
+    private TextField projectNameField;
+    @FXML
+    private TextField advisorField;
+    @FXML
+    private TextField advisorEmailField;
+    @FXML
+    private TextArea descriptionField;
+    @FXML
+    private TextField estNumField;
+    @FXML
+    private ComboBox<String> designationField;
+    @FXML
+    private ComboBox<String> majorField;
+    @FXML
+    private ComboBox<String> yearField;
+    @FXML
+    private ComboBox<String> departmentField;
+    @FXML
+    private ListView<String> categoryField;
 
-	Connection conn = null;
-	Statement stmt = null;
+    Connection conn = null;
+    Statement stmt = null;
 
-	// selected categories list
-	ArrayList<String> selected = new ArrayList<String>();
+    // selected categories list
+    ArrayList<String> selected = new ArrayList<String>();
 
-	// view for all activities
-	ObservableList<Activity> activities = FXCollections.observableArrayList();
+    // view for all activities
+    ObservableList<Activity> activities = FXCollections.observableArrayList();
 
-	@FXML
-	private void initialize() {
+    @FXML
+    private void initialize() {
 
-		descriptionField.setWrapText(true);
-		
-		// set category to be multiselect
-		categoryField.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        descriptionField.setWrapText(true);
 
-		// populate fields
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
+        // set category to be multiselect
+        categoryField.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
 
-			conn = DriverManager.getConnection(DB_URL, "cs4400_Team_1", "MONLSe9e");
-			stmt = conn.createStatement();
-			String sql;
+        // populate fields
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
 
-			// populate designation
-			sql = "SELECT designationName " 
-				+ "FROM DESIGNATION;";
-			ResultSet rs = stmt.executeQuery(sql);
-			while (rs.next()) {
-				designationField.getItems().add(rs.getString("designationName"));
-			}
+            conn = DriverManager.getConnection(DB_URL, "cs4400_Team_1", "MONLSe9e");
+            stmt = conn.createStatement();
+            String sql;
 
-			// populate majors
-			sql = "SELECT majorName " 
-				+ "FROM MAJOR;";
-			rs = stmt.executeQuery(sql);
-			while (rs.next()) {
-				majorField.getItems().add(rs.getString("majorName"));
-			}
+            // populate designation
+            sql = "SELECT designationName " + "FROM DESIGNATION;";
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                designationField.getItems().add(rs.getString("designationName"));
+            }
 
-			// populate category
-			sql = "SELECT categoryName " 
-				+ "FROM CATEGORY;";
-			rs = stmt.executeQuery(sql);
-			while (rs.next()) {
-				categoryField.getItems().add(rs.getString("categoryName"));
-			}
-			
-			// populate departments
-			departmentField.getItems().add("No requirement");
-			sql = "SELECT departmentName " 
-					+ "FROM DEPARTMENT;";
-			rs = stmt.executeQuery(sql);
-			while (rs.next()) {
-				departmentField.getItems().add(rs.getString("departmentName"));
-			}
+            // populate majors
+            sql = "SELECT majorName " + "FROM MAJOR;";
+            rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                majorField.getItems().add(rs.getString("majorName"));
+            }
 
-			rs.close();
-			stmt.close();
-			conn.close();
+            // populate category
+            sql = "SELECT categoryName " + "FROM CATEGORY;";
+            rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                categoryField.getItems().add(rs.getString("categoryName"));
+            }
 
-		} catch (SQLException se) {
-			se.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+            // populate departments
+            departmentField.getItems().add("No requirement");
+            sql = "SELECT departmentName " + "FROM DEPARTMENT;";
+            rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                departmentField.getItems().add(rs.getString("departmentName"));
+            }
 
-		// populate year (hardcoded)
-		yearField.getItems().addAll("FR", "SO", "JR", "SR");
-	}
-	
-	@FXML
-	public void handleSubmitPressed() {
-		
-		// check if all fields (besides department) needed for submit are empty
-		
-		if ((projectNameField.getText().isEmpty()
-				|| descriptionField.getText().isEmpty()
-				|| advisorField.getText().isEmpty()
-				|| advisorEmailField.getText().isEmpty()
-				|| advisorField.getText().isEmpty()
-				|| estNumField.getText().isEmpty()
-				|| categoryField.getSelectionModel().isEmpty()
-				|| designationField.getValue() == null
-				|| majorField.getValue() == null
-				|| yearField.getValue() == null)) {
-			Alert alert = new Alert(AlertType.INFORMATION);
-			alert.setTitle("Failure!");
-			alert.setHeaderText(null);
-			alert.setContentText("You have an empty field that needs to be filled out!");
-			alert.showAndWait();
-			System.out.println("You have an empty field that needs to be filled out!");
-		} else {
-			// check estNum field for integer value
-			try {
-				Integer.parseInt(estNumField.getText());
-			} catch (NumberFormatException er) {
-				Alert alert = new Alert(AlertType.INFORMATION);
-				alert.setTitle("Failure!");
-				alert.setHeaderText(null);
-				alert.setContentText("You did not put a valid number in the estNum field.");
-				alert.showAndWait();
-				System.out.println("You did not put a valid number in the estNum field.");
-				return;
-			} catch (Exception e) {
-				e.printStackTrace();
-				return;
-			}
-			
-			// insert into database
-			try {
-				Class.forName("com.mysql.jdbc.Driver");
-	
-				conn = DriverManager.getConnection(DB_URL, "cs4400_Team_1", "MONLSe9e");
-				stmt = conn.createStatement();
-				String sql;
-				ResultSet rs;
+            rs.close();
+            stmt.close();
+            conn.close();
 
-				// check if there is project with same project name first
-				sql = "SELECT projectName FROM PROJECT WHERE projectName = '" + projectNameField.getText() + "';";
-				rs = stmt.executeQuery(sql);
-				
-				if (rs.next()) {
-					Alert alert = new Alert(AlertType.INFORMATION);
-					alert.setTitle("Failure!");
-					alert.setHeaderText(null);
-					alert.setContentText("The project already exists!");
-					alert.showAndWait();
-					System.out.println("The project already exists!");
-				} else {
-					// insert into project table
-					sql = "INSERT INTO PROJECT VALUES ('"
-							+ projectNameField.getText() + "', '"
-							+ descriptionField.getText() + "', '"
-							+ estNumField.getText() + "', '"
-							+ advisorField.getText() + "', '"
-							+ advisorEmailField.getText() + "', '"
-							+ designationField.getValue() + "');";
-					stmt.executeUpdate(sql);
-					
-					// insert into project_category table
-					ObservableList<String> categories = categoryField.getSelectionModel().getSelectedItems();
-					for (String category: categories) {
-						sql = "INSERT INTO PROJECT_CATEGORY VALUES ('"
-								+ projectNameField.getText() + "', '"
-								+ category + "');";
-						stmt.executeUpdate(sql);
-					}
-					
-					// insert into the major_requirement table
-					sql = "INSERT INTO MAJOR_REQUIREMENT VALUES ('"
-							+ projectNameField.getText() + "', '"
-							+ majorField.getValue() + "');";
-					stmt.executeUpdate(sql);
-					
-					// insert into the year_requirement table
-					sql = "INSERT INTO YEAR_REQUIREMENT VALUES ('"
-							+ projectNameField.getText() + "', '"
-							+ yearField.getValue() + "');";
-					stmt.executeUpdate(sql);
-					
-					// insert into the department_requirement table if departmentField is not empty
-					if (!(departmentField.getValue() == null
-							|| departmentField.getValue().equals("No requirement"))) {
-						sql = "INSERT INTO DEPARTMENT_REQUIREMENT VALUES ('"
-								+ projectNameField.getText() + "', '"
-								+ departmentField.getValue() + "');";
-						stmt.executeUpdate(sql);
-					}
-					
-					Alert alert = new Alert(AlertType.INFORMATION);
-					alert.setTitle("Success!");
-					alert.setHeaderText(null);
-					alert.setContentText("Project has been added!");
-					alert.showAndWait();
-				}
-				
-				rs.close();
-				stmt.close();
-				conn.close();
-				
-			} catch (SQLException se) {
-				se.printStackTrace();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	
-	@FXML
-	public void handleBackPressed() {
-		showScreen("../view/AdminMainScreen.fxml", "Main Screen");
-	}
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // populate year (hardcoded)
+        yearField.getItems().addAll("FR", "SO", "JR", "SR");
+    }
+
+    @FXML
+    public void handleSubmitPressed() {
+
+        // check if all fields (besides department) needed for submit are empty
+
+        if (projectNameField.getText().isEmpty() || descriptionField.getText().isEmpty()
+                || advisorField.getText().isEmpty() || advisorEmailField.getText().isEmpty()
+                || advisorField.getText().isEmpty() || estNumField.getText().isEmpty()
+                || categoryField.getSelectionModel().isEmpty() || designationField.getSelectionModel().isEmpty()) {
+            alert("Error!", "All required fields not filled.");
+        } else {
+            // check estNum field for integer value
+            try {
+                Integer.parseInt(estNumField.getText());
+            } catch (NumberFormatException er) {
+                alert("Error!", "Invalid Number for Estimated Students.");
+                return;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return;
+            }
+
+            // insert into database
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+
+                conn = DriverManager.getConnection(DB_URL, "cs4400_Team_1", "MONLSe9e");
+                stmt = conn.createStatement();
+                String sql;
+
+                // insert into project table
+                sql = "INSERT INTO PROJECT VALUES ('" + projectNameField.getText() + "', '" + descriptionField.getText()
+                        + "', '" + estNumField.getText() + "', '" + advisorField.getText() + "', '"
+                        + advisorEmailField.getText() + "', '" + designationField.getValue() + "');";
+                stmt.executeUpdate(sql);
+
+                // insert into project_category table
+                ObservableList<String> categories = categoryField.getSelectionModel().getSelectedItems();
+                for (String category : categories) {
+                    sql = "INSERT INTO PROJECT_CATEGORY VALUES ('" + projectNameField.getText() + "', '" + category
+                            + "');";
+                    stmt.executeUpdate(sql);
+                }
+
+                // insert into the major_requirement table
+                if (!(majorField.getSelectionModel().isEmpty())) {
+                    sql = "INSERT INTO MAJOR_REQUIREMENT VALUES ('" + projectNameField.getText() + "', '"
+                            + majorField.getValue() + "');";
+                    stmt.executeUpdate(sql);
+                }
+                // insert into the year_requirement table
+                if (!(yearField.getSelectionModel().isEmpty())) {
+                    sql = "INSERT INTO YEAR_REQUIREMENT VALUES ('" + projectNameField.getText() + "', '"
+                            + yearField.getValue() + "');";
+                    stmt.executeUpdate(sql);
+                }
+                // insert into the department_requirement table if departmentField is not empty
+                if (!(departmentField.getSelectionModel().isEmpty())) {
+                    sql = "INSERT INTO DEPARTMENT_REQUIREMENT VALUES ('" + projectNameField.getText() + "', '"
+                            + departmentField.getValue() + "');";
+                    stmt.executeUpdate(sql);
+                }
+
+                alert("Success!", "Your project has been successfully added.");
+                
+                stmt.close();
+                conn.close();
+
+            } catch (SQLException se) {
+                if (se.getMessage().contains("Duplicate")) {
+                    alert("Course already exists!", "Cannot insert duplicate course number/course name.");
+                } else {
+                    se.printStackTrace();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @FXML
+    public void handleBackPressed() {
+        showScreen("../view/AdminMainScreen.fxml", "Main Screen");
+    }
 }
